ee1014fc5b781fe5614a23d800f286c6e10020aa
voiddev <brotales26@gmail.com>
2024-07-07 15:24:19 -0400

    som bug fixes

--- autoload/scene_manager.gd
+++ autoload/scene_manager.gd
@@ -6,14 +6,13 @@ func switch_scene(scene_path:String):
 	var dumb_shit = load(scene_path).instantiate()
 	trans.global_position.y = 720
 	var tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	tw.tween_property(trans,"global_position:y",-360,0.4)
+	tw.tween_property(trans,"global_position:y",-360,0.5)
 	await tw.finished
-	
+	get_tree().paused = true
 	get_tree().change_scene_to_packed(new_scene)
 	for i in 10:
 		await get_tree().physics_frame
 	tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	get_tree().paused = true
 	tw.tween_property(trans,"global_position:y",720,0.4)
 	await tw.finished
 	get_tree().paused = not get_window().has_focus()
