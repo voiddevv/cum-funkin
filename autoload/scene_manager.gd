6d429dc9efb322693f698851fa66148e321bdf23
what-is-a-git <138817584+what-is-a-git@users.noreply.github.com>
2024-07-08 12:10:25 -0400

    they COULD be trans

--- autoload/scene_manager.gd
+++ autoload/scene_manager.gd
@@ -1,20 +1,16 @@
-extends Node2D
+extends Node
 
-@onready var trans: TextureRect = $CanvasLayer/trans
+@onready var trans: TextureRect = $trans
 func switch_scene(scene_path:String):
-	var new_scene:PackedScene = load(scene_path)
-	var dumb_shit = load(scene_path).instantiate()
-	trans.global_position.y = 720
-	var tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	tw.tween_property(trans,"global_position:y",-360,0.5)
-	await tw.finished
 	get_tree().paused = true
-	get_tree().change_scene_to_packed(new_scene)
-	for i in 10:
-		await get_tree().physics_frame
-	tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	tw.tween_property(trans,"global_position:y",720,0.4)
+	trans.position.y = 720
+	var tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tw.tween_property(trans,"position:y",-360,0.5)
+	await tw.finished
+	get_tree().change_scene_to_file(scene_path)
+	await RenderingServer.frame_post_draw
+	await RenderingServer.frame_post_draw
+	tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tw.tween_property(trans,"position:y",720,0.5)
 	await tw.finished
 	get_tree().paused = not get_window().has_focus()
-	
-	
